955fbdff75a7a688dd97c660b69c66719d8a42c4
xingguangcuican6666 <xingguangcuican666@foxmail.com>
2025-10-19 18:39:15 +0800

    termux

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 chmod 777 * -R -v
+echo "ac_cv_lib_rt_mq_open=yes" > config.cache
 ac_cv_lib_rt_mq_open=yes LIBS="-lrt" LDFLAGS="-Wl,--no-as-needed" ./configure --prefix=$PREFIX
-make -j8
-make install
